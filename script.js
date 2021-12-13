16b78f0486e9ed5d7f56df3b42085b7c73d2367b
ElAmirr <othmaniamir41@gmail.com>
2021-12-13 09:52:46 +0100

    push

--- script.js
+++ script.js
@@ -27,4 +27,20 @@ function showDivs(n) {
   x[slideIndex-1].style.display = "block";  
 }
 
+// bg ayto slide
+var myIndexx = 0;
+carousel();
+
+function carousel() {
+  var ix;
+  var xx = document.getElementsByClassName("bg-title");
+  for (ix = 0; ix < xx.length; ix++) {
+    xx[ix].style.display = "none";  
+  }
+  myIndexx++;
+  if (myIndexx > xx.length) {myIndexx = 1}    
+  xx[myIndexx-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
 // Animations
